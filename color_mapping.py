2b246c3ced36c347416e21ba40b0eda97f5f75c9
Anthony_Kalampogias <antonykalampogias@gmail.com>
2023-04-22 22:05:16 +0200

    more bug fixes

--- color_mapping.py
+++ color_mapping.py
@@ -1,15 +1,5 @@
 import random
 
-nodes = [i for i in range(16)]
-colors = ['B', 'R', 'G', 'Y']
-edges = [(0, 1), (0, 2), (0, 3), (0, 12), (0, 14), (0, 15), (1, 2), (1, 4), (1, 8), (1, 7), (1, 13), (1, 14), (1, 15), (2, 3), (2, 4), (2, 5), (3, 5), (4, 5), (4, 6), (4, 8), (4, 9),
-         (5, 6), (5, 10), (5, 12), (6, 9), (6, 10), (7, 8), (7, 13), (8, 9), (8, 11), (8, 13), (9, 10), (9, 11), (10, 11), (10, 12), (11, 12), (11, 13), (11, 14), (12, 14), (13, 14), (14, 15)]
-population = {}
-# if the fitness is equal to the fitness goal, then the solution is found
-fitness_goal = len(edges)
-N = 10  # size of the population
-
-
 #                                                       #
 # Fitness function to evaluate each candidate solutions #
 #                                                       #
@@ -20,7 +10,6 @@ def fitness_function(solution):
         if solution[edges[i][0]] != solution[edges[i][1]]:
             fitness += 1
     return fitness
-
 
 #                   #
 # Parent selection  #
@@ -46,8 +35,6 @@ def roulette_selection(population):
 #
 # Reproduction (using single-point crossover)
 #
-
-
 def crossover(parent1, parent2):
     n = len(parent1)
     child1 = parent1[:n//2]+parent2[n//2:]
@@ -64,13 +51,13 @@ def initialize(N, nodes, colors):
         population[s] = fitness_function(s)
     return population
 
-def algorithm(N, nodes, colors, fitness_goal):
-    # random initial population (size=10)
+def algorithm(nodes, colors, edges, N, repeat_limit=10000):
+    # random initial population (size=N)
     population = initialize(N, nodes, colors)
-
+    fitness_goal = len(edges) # all the nodes of the graph that are in the same edge have different colors
     counter = 0
-    # while the fitness is not equal to the fitness goal
-    while (max(population.values()) < fitness_goal and counter < 20000):
+    # while the fitness is not equal to the fitness goal(we haven't found a solution yet)
+    while (max(population.values()) < fitness_goal and counter < repeat_limit):
         counter += 1
         for j in range(N//2):
             # select two parents
@@ -81,16 +68,25 @@ def algorithm(N, nodes, colors, fitness_goal):
             # mutate
             # child1=mutate(child1)
             # child2=mutate(child2)
+
             # evaluate the fitness of the children
             if child1 not in population:
                 population[child1] = fitness_function(child1)
             if child2 not in population:
                 population[child2] = fitness_function(child2)
-        # select the best 10 solutions
-        population = dict(sorted(population.items(),key=lambda item: item[1], reverse=True)[:10])
+        # select the best N solutions
+        population = dict(sorted(population.items(),key=lambda item: item[1], reverse=True)[:N])
     best_solutions = [k for k, v in population.items() if v == max(population.values())]
     print(f"The best solution/solutions with a fitness score of {max(population.values())}: {best_solutions}")
     
+#
+# Our data 
+#
+nodes = [i for i in range(16)]
+colors = ['B', 'R', 'G', 'Y']
+edges = [(0, 1), (0, 2), (0, 3), (0, 12), (0, 14), (0, 15), (1, 2), (1, 4), (1, 8), (1, 7), (1, 13), (1, 14), (1, 15), (2, 3), (2, 4), (2, 5), (3, 5), (4, 5), (4, 6), (4, 8), (4, 9),
+         (5, 6), (5, 10), (5, 12), (6, 9), (6, 10), (7, 8), (7, 13), (8, 9), (8, 11), (8, 13), (9, 10), (9, 11), (10, 11), (10, 12), (11, 12), (11, 13), (11, 14), (12, 14), (13, 14), (14, 15)]
+N = 10  # size of the population
+repeat_limit=50000 #a limit in order to avoid endless repeats (in some cases the algorithm cannot find an optimal solution)
 
-
-algorithm(N, nodes, colors, fitness_goal)
+algorithm(nodes, colors, edges, N, repeat_limit)
